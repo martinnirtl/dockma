be26b59bb82246e45d8316d3e4e1ff0cf8fd7951
Martin Nirtl <martin.nirtl@gmail.com>
2020-01-31 18:58:42 +0100

    add dockercompose pkg incl ReadServices func

--- pkg/dockercompose/dockercompose.go
+++ pkg/dockercompose/dockercompose.go
@@ -1,1 +1,28 @@
 package dockercompose
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// ReadServices returns defined services of docker-compose files
+func ReadServices(filepath string) (services []string, err error) {
+
+	os.Chdir(filepath)
+
+	out, e := exec.Command("docker-compose", "config", "--services").Output()
+
+	if e != nil {
+		err = errors.New("Could not execute docker-compose")
+	} else {
+		for _, service := range strings.Split(string(out), "\n") {
+			if service != "" {
+				services = append(services, service)
+			}
+		}
+	}
+
+	return
+}
