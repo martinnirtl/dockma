4f7eb07a435c7af56bd57736107090e2aa956ffb
Martin Nirtl <martin.nirtl@gmail.com>
2020-02-02 00:00:28 +0100

    finish config cat cmd

--- internal/commands/configcmd/cat.go
+++ internal/commands/configcmd/cat.go
@@ -1,14 +1,32 @@
 package configcmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/martinnirtl/dockma/internal/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/ttacon/chalk"
+)
 
 var catCmd = &cobra.Command{
 	Use:     "cat",
-	Short:   "Concatinate config.json of dockma",
+	Short:   "Print config.json of dockma",
 	Long:    `-`,
 	Example: "dockma config cat",
 	Run: func(cmd *cobra.Command, args []string) {
+		filepath := utils.GetFullLogfilePath("config.json")
 
+		content, err := ioutil.ReadFile(filepath)
+
+		if err != nil {
+			fmt.Printf("%sError: %s%s\n", chalk.Red, viper.GetString("username"), chalk.ResetColor)
+		} else {
+			fmt.Printf("%sHere comes the configuration file of dockma:%s\n", chalk.Cyan, chalk.ResetColor)
+
+			fmt.Println(string(content))
+		}
 	},
 }
 
