f7b29da9cf75adf30b8777362e0d8dc2b6338d4a
Martin Nirtl <martin.nirtl@gmail.com>
2020-02-18 22:32:19 +0100

    clean completion cmd

--- internal/commands/completioncmd/root.go
+++ internal/commands/completioncmd/root.go
@@ -2,9 +2,7 @@ package completioncmd
 
 import (
 	"errors"
-	"fmt"
 	"os"
-	"strings"
 
 	"github.com/martinnirtl/dockma/internal/survey"
 	"github.com/martinnirtl/dockma/internal/utils"
@@ -15,20 +13,17 @@ var shells []string = []string{"bash", "powershell", "zsh"}
 
 // CompletionCommand implements the top level install command
 var CompletionCommand = &cobra.Command{
-	Use:     "completion [shell]",
-	Short:   "Generate shell completion code",
-	Long:    "Generate shell completion code",
-	Example: "dockma completion bash",
-	Args:    cobra.RangeArgs(0, 1),
+	Use:       "completion [shell]",
+	Short:     "Generate shell completion code",
+	Long:      "Generate shell completion code",
+	Example:   "dockma completion bash",
+	Args:      cobra.OnlyValidArgs,
+	ValidArgs: shells,
 	Run: func(cmd *cobra.Command, args []string) {
 		var shell string
 
 		if len(args) > 0 {
-			if utils.Includes(shells, args[0]) {
-				shell = args[0]
-			} else {
-				utils.ErrorAndExit(fmt.Errorf("Provided type not supported [%s]", strings.Join(shells, "|")))
-			}
+			shell = args[0]
 		} else {
 			shell = survey.Select("Select shell to install completion for", shells)
 		}
