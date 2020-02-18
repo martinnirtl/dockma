3f77346e17c1576bc768ccd6df31a75272c154ec
Martin Nirtl <martin.nirtl@gmail.com>
2020-02-18 09:32:44 +0100

    add todo in code

--- cmd/dockma.go
+++ cmd/dockma.go
@@ -4,6 +4,13 @@ import (
 	"github.com/martinnirtl/dockma/internal/commands"
 )
 
+// TODO set version, commit and date with build flags and in dev mode
+// var (
+// 	version = "dev"
+// 	commit  = "none"
+// 	date    = "unknown"
+// )
+
 func main() {
 	commands.Execute()
 }
