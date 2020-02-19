16e298eeb8d954e5812798cd61951da04f85ed3b
Martin Nirtl <martin.nirtl@gmail.com>
2020-02-19 10:08:57 +0100

    enable pull cmd to work on all envs

--- internal/commands/envcmd/pull.go
+++ internal/commands/envcmd/pull.go
@@ -7,29 +7,32 @@ import (
 
 	"github.com/martinnirtl/dockma/internal/config"
 	"github.com/martinnirtl/dockma/internal/utils"
+	"github.com/martinnirtl/dockma/internal/utils/helpers"
 	"github.com/martinnirtl/dockma/pkg/externalcommand"
 	"github.com/martinnirtl/dockma/pkg/externalcommand/spinnertimebridger"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"github.com/ttacon/chalk"
 )
 
 var pullCommand = &cobra.Command{
 	Use:     "pull",
-	Short:   "Run 'git pull' in active environment home dir",
-	Long:    "Run 'git pull' in active environment home dir",
+	Short:   "Run 'git pull' in environment home dir",
+	Long:    "Run 'git pull' in environment home dir",
 	Example: "dockma env pull",
-	Args:    cobra.NoArgs,
+	Args:    cobra.RangeArgs(0, 1),
 	Run: func(cmd *cobra.Command, args []string) {
-		activeEnv := config.GetActiveEnv()
-
-		if activeEnv.GetName() == "-" {
-			utils.PrintNoActiveEnvSet()
+		envName := ""
+		if len(args) == 0 {
+			envName = helpers.GetEnvironment("")
+		} else {
+			envName = helpers.GetEnvironment(args[0])
 		}
+		env, err := config.GetEnv(envName)
+		utils.ErrorAndExit(err)
 
-		envHomeDir := activeEnv.GetHomeDir()
+		envHomeDir := env.GetHomeDir()
 
-		hideCmdOutput := viper.GetBool("hidesubcommandoutput")
+		hideCmdOutput := config.GetHideSubcommandOutputSetting()
 
 		output, err := Pull(envHomeDir, hideCmdOutput, true)
 		if err != nil && hideCmdOutput {
@@ -37,7 +40,7 @@ var pullCommand = &cobra.Command{
 		}
 		utils.ErrorAndExit(err)
 
-		utils.Success(fmt.Sprintf("Pulled env: %s", activeEnv.GetName()))
+		utils.Success(fmt.Sprintf("Pulled env: %s", env.GetName()))
 	},
 }
 
