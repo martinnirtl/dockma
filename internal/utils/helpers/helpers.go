966b0c3d17e6ce4613f3022feb8380cf158e18f5
Martin Nirtl <martin@nirtl.rocks>
2020-03-20 15:27:05 +0100

    use utils functions instead of custom printf

--- internal/utils/helpers/helpers.go
+++ internal/utils/helpers/helpers.go
@@ -3,19 +3,20 @@ package helpers
 import (
 	"fmt"
 
+	"github.com/martinnirtl/dockma/internal/utils"
 	"github.com/ttacon/chalk"
 )
 
 // PrintErrorList prints error list as red 'suberrors' to stdout (To be used with config.SaveNow).
 func PrintErrorList(errorList []error) {
 	for _, err := range errorList {
-		fmt.Printf("%s> %s%s\n", chalk.Red, err, chalk.ResetColor)
+		fmt.Println(chalk.Red.Color(fmt.Sprintf("> %s", err)))
 	}
 }
 
 // PrintMessageList prints messages to stdout (To be used with config.SaveNow)
 func PrintMessageList(messageList []string) {
 	for _, msg := range messageList {
-		fmt.Printf("%s\n", msg)
+		utils.Success(msg)
 	}
 }
