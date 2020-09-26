f3fdc26f26e6560e8bc651ae45d7f8c63a77a54a
Martin Nirtl <martin@nirtl.rocks>
2020-09-26 13:32:44 +0200

    rm commented code

--- cmd/dockma.go
+++ cmd/dockma.go
@@ -6,13 +6,6 @@ import (
 	"github.com/martinnirtl/dockma/internal/commands"
 )
 
-// TODO set version, commit and date with build flags and in dev mode
-// var (
-// 	version = "dev"
-// 	commit  = "none"
-// 	date    = "unknown"
-// )
-
 func main() {
 	log.SetPrefix("dockma: ")
 
